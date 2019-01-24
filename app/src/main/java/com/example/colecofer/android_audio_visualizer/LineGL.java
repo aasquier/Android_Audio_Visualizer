3f107844f82ba8254929265c32c968720eeed73c
Cole Cofer <coleocofer@gmail.com>
2019-01-23 23:23:30 -0800

    Added a LineGL class for line rendering

---
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/LineGL.java
@@ -0,0 +1,17 @@
+package com.example.colecofer.android_audio_visualizer;
+
+import java.nio.FloatBuffer;
+
+public class LineGL {
+
+    //FloatBuffer's are used for efficiency
+    private FloatBuffer vertexBuffer;
+    private int mProgram;
+
+    //Drawing Variables
+    private int positionHandle;
+    private int colorHandle;
+
+//    private final int vertexCount =
+
+}
