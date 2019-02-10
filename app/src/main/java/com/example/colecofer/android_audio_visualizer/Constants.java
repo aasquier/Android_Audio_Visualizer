d5c6aa447839eaa22e65a9f57518d8177b8027dd
aasquier <aasquier@pdx.edu>
2019-02-10 01:30:21 -0800

    Stable fractal noise with basic raindrop effect for vis2

--- app/src/main/java/com/example/colecofer/android_audio_visualizer/Constants.java
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/Constants.java
@@ -60,8 +60,8 @@ final class Constants {
     /** Vis2 constants */
     static final int VIS2_STRIDE_BYTES = (POSITION_DATA_SIZE + COLOR_DATA_SIZE) * BYTES_PER_FLOAT;
     /** GLDot constants for Vis2*/
-    static final int DOT_HEIGHT = 600;
-    static final int DOT_WIDTH  = 600;
+    static final int DOT_HEIGHT = 2000;
+    static final int DOT_WIDTH  = 2000;
     static final int DOT_COUNT  = DOT_WIDTH * DOT_HEIGHT;
 
     /** VisualizerActivity constants */
--- app/src/main/res/raw/vistwovertex.glsl
+++ app/src/main/res/raw/vistwovertex.glsl
@@ -48,7 +48,83 @@ varying vec4   v_Color;         // This will be passed into the fragment shader 
 
 uniform float time;
 
+//vec2 Hash(vec2 P)
+//{
+// 	return fract(cos(P*mat2(-64.2,71.3,81.4,-29.8))*8321.3);
+//}
+//float Worley(vec2 P)
+//{
+//    float Dist = 1.;
+//    vec2 I = floor(P);
+//    vec2 F = fract(P);
+//
+//    for(int X = -1;X<=1;X++)
+//    for(int Y = -1;Y<=1;Y++)
+//    {
+//        float D = distance(Hash(I+vec2(X,Y))+vec2(X,Y),F);
+//        Dist = min(Dist,D);
+//    }
+//    return Dist;
+//}
+
+float hash(float n) {
+ 	return fract(cos(n*89.42)*343.42);
+}
+
+vec2 hash2(vec2 n) {
+ 	return vec2(hash(n.x*23.62-300.0+n.y*34.35),hash(n.x*45.13+256.0+n.y*38.89));
+}
+
+float worley(vec2 c, float time) {
+    float dis = 1.0;
+    for(int x = -1; x <= 1; x++)
+        for(int y = -1; y <= 1; y++){
+            vec2 p = floor(c)+vec2(x,y);
+            vec2 a = hash2(p) * time;
+            vec2 rnd = 0.5+sin(a)*0.5;
+            float d = length(rnd+vec2(x,y)-fract(c));
+            dis = min(dis, d);
+        }
+    return dis;
+}
+
+float worley5(vec2 c, float time) {
+    float w = 0.0;
+    float a = 0.5;
+    for (int i = 0; i<5; i++) {
+        w += worley(c, time)*a;
+        c*=2.0;
+        time*=2.0;
+        a*=0.5;
+    }
+    return w;
+}
+
+
+
 void main() {
+//    vec2 res = vec2(1.0, 1.0);
+
+
+    float dis = worley5(a_Position.xy/0.35, (time/1500.0));
+    vec3 c = mix(vec3(1.0,0.95,0.5), vec3(0.7,0.0,0.0), dis);
+    v_Color = vec4(c*c, 1.0);
+
+    float scaledTime = time / 1000.0;
+
+    vec2 res = vec2(0.95, 0.95);
+    vec2 cPos = vec2(2.0 * (a_Position.xy / res.xy));
+    float cLength = length(cPos);
+    vec2 uv = (a_Position.xy / res.xy) + (cPos / cLength) * sin(cLength * 12.0 - scaledTime * 4.0) * 0.03;
+    vec4 newPosition = vec4(uv, a_Position.zw);
+
+//    v_Color = a_Color;
+//    gl_Position = a_Position;
+	gl_Position = newPosition;
+    gl_PointSize = 1.0;// + a_DB_Level;        // This will adjust the dot size from 1.0-2.0 based on decibel level which is in the range 0.0-1.0
+
+
+// TODO Graveyard of former shader rejects
 //    float nx = ((a_Position.x*a_DB_Level)) - 0.5;
 //    float ny = ((a_Position.y*a_DB_Level)) - 0.5;
 //    vec2 noiseVec = vec2(nx, ny);
@@ -58,16 +134,16 @@ void main() {
 //    vec4 newPosition = vec4(snoise(nx, ny), snoise(nx, ny), a_Position.zw);
 //    v_Color = newColor;
 
-    float scaledTime = time / 250.0;
-
-    vec2 res = vec2(0.95, 0.95);
-    vec2 cPos = vec2(2.0 * (a_Position.xy / res.xy));
-    float cLength = length(cPos);
-    vec2 uv = (a_Position.xy / res.xy) + (cPos / cLength) * sin(cLength * 12.0 - scaledTime * 4.0) * 0.03;
-    vec4 newPosition = vec4(uv, a_Position.zw);
-
-    v_Color = a_Color;
-    gl_Position = newPosition;	        // gl_Position is a special variable used to store the final position for the fragment shader
-//    gl_Position = a_Position;
-    gl_PointSize = 1.0 + a_DB_Level;        // This will adjust the dot size from 1.0-2.0 based on decibel level which is in the range 0.0-1.0
+//    float scaledTime = time / 250.0;
+//
+//    vec2 res = vec2(0.95, 0.95);
+//    vec2 cPos = vec2(2.0 * (a_Position.xy / res.xy));
+//    float cLength = length(cPos);
+//    vec2 uv = (a_Position.xy / res.xy) + (cPos / cLength) * sin(cLength * 12.0 - scaledTime * 4.0) * 0.03;
+//    vec4 newPosition = vec4(uv, a_Position.zw);
+//
+//    v_Color = a_Color;
+//    gl_Position = newPosition;	        // gl_Position is a special variable used to store the final position for the fragment shader
+////    gl_Position = a_Position;
+//    gl_PointSize = 1.0 + a_DB_Level;        // This will adjust the dot size from 1.0-2.0 based on decibel level which is in the range 0.0-1.0
 }
