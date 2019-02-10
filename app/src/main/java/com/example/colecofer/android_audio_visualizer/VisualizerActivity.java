7d4d796f1c362b92dcd6d4288010446d51ad5889
Daniel Elias Bermudez <de.bermudez13@gmail.com>
2019-02-09 19:45:19 -0800

    Remove unused imports

--- app/src/main/java/com/example/colecofer/android_audio_visualizer/VisualizerActivity.java
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/VisualizerActivity.java
@@ -11,14 +11,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.spotify.sdk.android.player.SpotifyPlayer;
