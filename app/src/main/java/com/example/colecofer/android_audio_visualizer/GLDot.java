5fdf6388099b7093a43e5a96d7b2d7b6e64028e2
Daniel Elias Bermudez <de.bermudez13@gmail.com>
2019-02-15 10:31:16 -0800

    Add colors to visualizer 2

--- app/src/main/java/com/example/colecofer/android_audio_visualizer/GLDot.java
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/GLDot.java
@@ -1,9 +1,10 @@
 package com.example.colecofer.android_audio_visualizer;
 
+import android.graphics.Color;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
-import java.util.ArrayDeque;
 import java.util.Random;
 
 import static com.example.colecofer.android_audio_visualizer.Constants.DOT_COUNT;
@@ -22,15 +23,18 @@ public class GLDot {
 
         int index = 0;
 
+        int visTwoIndex = 1;
+        int visColor = VisualizerModel.getInstance().getColor(visTwoIndex);
+
         for(int i = 0; i < DOT_HEIGHT; i++) {
 
             for(int j = 0; j < DOT_WIDTH; j++) {
                 vertices[index*7+0] = (float)(-1.0 + 2.0 /(DOT_HEIGHT + 1)*(1+i));
                 vertices[index*7+1] = (float)(-1.0 + 2.0 /(DOT_WIDTH + 1)*(1+j));
                 vertices[index*7+2] = 0.0f;
-                vertices[index*7+3] = 1.0f;
-                vertices[index*7+4] = 0.0f;
-                vertices[index*7+5] = 0.0f;
+                vertices[index*7+3] = (float) (Color.red(visColor) * 0.01);
+                vertices[index*7+4] = (float) (Color.green(visColor) * 0.01);
+                vertices[index*7+5] = (float) (Color.blue(visColor) * 0.01);
                 vertices[index*7+6] = 1.0f;
 
                 index++;
