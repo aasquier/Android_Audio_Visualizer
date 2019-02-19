c9b4898f2e8f6e134015acb2c572f2e78c1bf5dc
Cole Cofer <coleocofer@gmail.com>
2019-02-18 21:16:47 -0800

    Created dispalyLyrics function

--- app/src/main/java/com/example/colecofer/android_audio_visualizer/AnimateLyrics.java
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/AnimateLyrics.java
@@ -7,40 +7,68 @@ import android.support.v4.content.res.ResourcesCompat;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.util.Pair;
 import android.util.TypedValue;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import static com.example.colecofer.android_audio_visualizer.Constants.LYRICS_TEXT_SIZE;
 
+
+/**
+ * This class creates lyric animations using Spannable Strings.
+ * These are useful because you can treat words individually,
+ * where you can't with normal Strings.
+ * The animation consists of multiple Spannable Strings kept
+ * inside of a TextView.
+ */
 public class AnimateLyrics {
 
     private Typeface lyricsTypeface;
 
-    static TextView lyrics;
+    static TextView lyricsTextView;
     static ViewGroup.MarginLayoutParams lyricsParams;
 
+    private Spannable lyrics[];
 
     public AnimateLyrics(Context context) {
-        lyricsTypeface = ResourcesCompat.getFont(context, R.font.sofiaproextralight);
-        lyrics = new TextView(context);
-        lyrics.setTextSize(TypedValue.COMPLEX_UNIT_SP, LYRICS_TEXT_SIZE);
-        lyrics.setTypeface(lyricsTypeface);
+        lyricsTypeface = ResourcesCompat.getFont(context, R.font.sofiaprobold);
+        lyricsTextView = new TextView(context);
+        lyricsTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, LYRICS_TEXT_SIZE);
+        lyricsTextView.setTypeface(lyricsTypeface);
+        lyricsTextView.setTextColor(Color.WHITE);
 
-        //Setup the text and colors
-        Spannable word = new SpannableString("Hah, sika than your average\n");
-
-        //The first two value of the hex are opacity... So perhaps we could alter these to fade them in and out... ?
-        word.setSpan(new ForegroundColorSpan(0x50FFFFFF), 0, word.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        lyrics.setText(word);
-
-        Spannable word1 = new SpannableString("Poppa twist cabbage off instinct");
-        word1.setSpan(new ForegroundColorSpan(Color.RED), 0, word1.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        lyrics.append(word1);
+//        //Setup the text and colors
+//        Spannable word = new SpannableString("Hah, sika than your average\n");
+//
+//        //The first two value of the hex are opacity... So perhaps we could alter these to fade them in and out... ?
+//        word.setSpan(new ForegroundColorSpan(0x50FFFFFF), 0, word.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        lyricsTextView.setText(word);
+//
+//        Spannable word1 = new SpannableString("Poppa twist cabbage off instinct");
+//        word1.setSpan(new ForegroundColorSpan(Color.RED), 0, word1.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        lyricsTextView.append(word1);
 
         lyricsParams = new ViewGroup.MarginLayoutParams(ViewGroup.MarginLayoutParams.MATCH_PARENT, ViewGroup.MarginLayoutParams.MATCH_PARENT);
-        lyrics.setLayoutParams(lyricsParams);
-        lyrics.setPadding(100, 800, 100, 300);
+        lyricsTextView.setLayoutParams(lyricsParams);
+        lyricsTextView.setPadding(100, 800, 100, 300);
     }
+
+    /**
+     * Takes an array of lyrics and displays them to
+     * the lyric animation TextView.
+     * @param lyrics String array of lyrics (word by word)
+     */
+    public void displayLyrics(String[] lyrics) {
+        int wordsAmt = lyrics.length;
+        String lyricsToDisplay = "";
+        for (int i = 0; i < wordsAmt; ++i) {
+            lyricsToDisplay += lyrics[i];
+        }
+        this.lyricsTextView.setText(lyricsToDisplay);
+    }
+
 
 }
