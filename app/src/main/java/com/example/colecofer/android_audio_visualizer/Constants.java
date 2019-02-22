146db0a0d2e9bb225cb1d0ebbda871a7fbf74fc8
Lydeon <leonlee190@gmail.com>
2019-02-22 11:13:14 -0800

    blending

--- app/src/main/java/com/example/colecofer/android_audio_visualizer/Constants.java
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/Constants.java
@@ -11,7 +11,7 @@ final class Constants {
     static final int BYTES_PER_FLOAT = 4;
 
     /** Visualizer Switching */
-    static final boolean SHOULD_SWITCH_VIS = true;                       //Set to false if you do not want to rotate visualizers
+    static final boolean SHOULD_SWITCH_VIS = false;                       //Set to false if you do not want to rotate visualizers
     static final boolean SHOULD_LOOP_VIS   = true;                       //Set to true if you want the final visualizer to loop back to the first
     static final int SWITCH_VIS_TIME       = 5000;                      //Number of milliseconds to wait before switching
 
@@ -55,7 +55,7 @@ final class Constants {
     static final String GLSL_TIME            = "time";
 
     /** Vis1 constants */
-    static final int LINE_AMT              = 50;                  //Number of lines to display on the screen
+    static final int LINE_AMT              = 30;                  //Number of lines to display on the screen
     static final float AMP_MULT            = 0.000005f;           //Alters the lines horizontal amplitude
     static final int VERTEX_AMOUNT         = 7;                   //x, y, z, r, g, b, a
     static final float LEFT_DRAW_BOUNDARY  = -0.99f;              //Where to start drawing on the left side of the screen
