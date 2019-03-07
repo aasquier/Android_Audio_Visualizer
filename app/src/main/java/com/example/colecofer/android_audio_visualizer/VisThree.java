e743d725a15b3dd29abef027f0a2301d4d076fa8
lwarden <lwarden@pdx.edu>
2019-03-06 18:46:32 -0800

    Fractal wave now uses average decibel

--- app/src/main/java/com/example/colecofer/android_audio_visualizer/VisThree.java
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/VisThree.java
@@ -81,7 +81,9 @@ public class VisThree extends VisualizerBase {
      * This runs once per draw cycle
      */
     public void updateFractalLineArray(){
-        if(decibelHistory.peek() > .7f){
+        Float[] decibelHistoryArray = decibelHistory.toArray(new Float[0]);
+        double averageDecibel = (decibelHistoryArray[0] + decibelHistoryArray[1] + decibelHistoryArray[2] + decibelHistoryArray[3] + decibelHistoryArray[4]) / 5.0;
+        if(averageDecibel > .6){
             lineFractalStrength[LINE_AMT_V3 - 1] = 4;
         } else {
             if (lineFractalStrength[LINE_AMT_V3 - 1] != 0)
