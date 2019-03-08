befdbb34f2e61d8ed4c020f4b2e01368922152c4
Cole Cofer <coleocofer@gmail.com>
2019-03-07 21:58:25 -0800

    Added demo mode

--- app/src/main/java/com/example/colecofer/android_audio_visualizer/MusixmatchClient.java
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/MusixmatchClient.java
@@ -118,9 +118,12 @@ public class MusixmatchClient {
         //JSONObject json = SpotifyClient.convertStringToJSON(responseJSON);
         //String fullString = "Error - Could not extract lyrics";
 
-        String fullString = "[00:00.30] One, two, three!\n[00:01.53] My baby don't mess around\n[00:03.60] Because she loves me so\n[00:05.22] This I know fo sho!\n[00:09.74] But does she really wanna\n[00:12.14] But can't stand to see me walk out the door\n[00:18.04] Don't try to fight the feeling\n[00:20.61] Because the thought alone is killin' me right now\n[00:26.56] Thank God for Mom and Dad\n[00:28.78] For sticking to together\n[00:30.46] Like we don't know how\n[00:34.39] Hey ya! Hey ya!\n[00:41.57] Hey ya! Hey ya!\n[00:48.45] Hey ya! Hey ya!\n[00:55.38] Hey ya! Hey ya!\n[01:07.95] You think you've got it\n[01:09.03] Oh, you think you've got it\n[01:10.52] But got it just don't get it when there's nothin' at all\n[01:16.18] We get together\n[01:17.27] Oh, we get together\n[01:18.74] But separate's always better when there's feelings involved";
-        //try {
-        //fullString = json.getJSONObject("Lyrics").getString("subtitle_body");
+        //Demo lyrics
+        //String fullString = "[00:00.30] One, two, three!\n[00:01.53] My baby don't mess around\n[00:03.60] Because she loves me so\n[00:05.22] This I know fo sho!\n[00:09.74] But does she really wanna\n[00:12.14] But can't stand to see me walk out the door\n[00:18.04] Don't try to fight the feeling\n[00:20.61] Because the thought alone is killin' me right now\n[00:26.56] Thank God for Mom and Dad\n[00:28.78] For sticking to together\n[00:30.46] Like we don't know how\n[00:34.39] Hey ya! Hey ya!\n[00:41.57] Hey ya! Hey ya!\n[00:48.45] Hey ya! Hey ya!\n[00:55.38] Hey ya! Hey ya!\n[01:07.95] You think you've got it\n[01:09.03] Oh, you think you've got it\n[01:10.52] But got it just don't get it when there's nothin' at all\n[01:16.18] We get together\n[01:17.27] Oh, we get together\n[01:18.74] But separate's always better when there's feelings involved";
+        String fullString = BIGGIE_LYRICS;
+
+//        try {
+//        fullString = json.getJSONObject("Lyrics").getString("subtitle_body");
                 String lyricStrings[] = fullString.replaceFirst("\\[..:..\\...] ", "").split("\\[..:..\\...] "); //separates lyrics from [##:##.##] timestamps. Removes first instance of delimiter to avoid leading empty string
                 String timestampStrings[] = fullString.split("\\s.*(\\n|$)");  //extract [##:##.##] from lyrics
                 if (lyricStrings.length != timestampStrings.length) {
@@ -137,16 +140,17 @@ public class MusixmatchClient {
                     Pair currPair = new Pair(durationInMillis, lyricStrings[i].split(" "));
 
                     lyricList.add(currPair);
-                //}
-                //} catch (JSONException e) {
-                //Log.d("MusixMatch", "Error - Could not extract lyrics from response" + e.getMessage());
-                //return null;
+                }
+//                } catch (JSONException e) {
+                Log.d("MusixMatch", "Error - Could not extract lyrics from response" + e.getMessage());
+                return null;
             }
 
                 return lyricList;
     }
 
-private String BIGGIE_LYRICS =
+    //These are strictly for demo purposes only - since we don't have a musixmatch license.
+    private static String BIGGIE_LYRICS =
         "[00:01.5] Uhhh, uhhh, uh, c`mon\n" +
         "[00:06.5] Hah,\t" +
         "sicka than your average\n" +
