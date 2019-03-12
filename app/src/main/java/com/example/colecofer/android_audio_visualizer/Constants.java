acda9510c198985a097d58d471727c55cbd17ac3
Cole Cofer <coleocofer@gmail.com>
2019-03-11 19:55:53 -0700

    Added grand total

--- app/src/main/java/com/example/colecofer/android_audio_visualizer/Constants.java
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/Constants.java
@@ -5,7 +5,7 @@ final class Constants {
     /** Visualizer Switching */
     static final boolean SHOULD_SWITCH_VIS = true;          //Set to false if you do not want to rotate visualizers
     static final boolean SHOULD_LOOP_VIS   = true;          //Set to true if you want the final visualizer to loop back to the first
-    static final int SWITCH_VIS_TIME       = 5000;          //Number of milliseconds to wait before switching
+    static final int SWITCH_VIS_TIME       = 10000;          //Number of milliseconds to wait before switching
 
     /** VisualizerActivity constants */
     static final int REQUEST_PERMISSION     = 101;
--- app/src/main/java/com/example/colecofer/android_audio_visualizer/VisualizerModel.java
+++ app/src/main/java/com/example/colecofer/android_audio_visualizer/VisualizerModel.java
@@ -80,7 +80,7 @@ public class VisualizerModel {
     public void initVisualizerQueue() {
         visQueue = new ConcurrentLinkedQueue<>();
         visQueue.add(visOne);
-        visQueue.add(visTwo);
+//        visQueue.add(visTwo);
         visQueue.add(visThree);
 
         visCount = visQueue.size();
